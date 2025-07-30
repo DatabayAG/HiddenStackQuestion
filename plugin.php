ebc2232ea485d6d55cefc1881960f7632b7bf65c
Marvin Beym <mBeym@databay.de>
2025-07-30 08:30:46 +0200

    Update versions

--- plugin.php
+++ plugin.php
@@ -2,8 +2,8 @@
 
 /* Copyright (c) 1998-2017 ILIAS open source, Extended GPL, see docs/LICENSE */
 $id = 'hsqst';
-$version = '2.0.0';
-$ilias_min_version = '8.0';
-$ilias_max_version = '8.999';
+$version = '9.0.0';
+$ilias_min_version = '9.0';
+$ilias_max_version = '9.999';
 $responsible = 'Databay AG';
 $responsible_mail = 'mjansen@databay.de';
